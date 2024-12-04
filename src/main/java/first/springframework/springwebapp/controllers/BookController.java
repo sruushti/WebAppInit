4dab15ad5e2019588b1a6960ba155de1759b65a4
Srushti S <srushhhti07@gmail.com>
2024-12-04 19:53:40 +0530

    controller

---
+++ src/main/java/first/springframework/springwebapp/controllers/BookController.java
@@ -0,0 +1,28 @@
+package first.springframework.springwebapp.controllers;
+
+import first.springframework.springwebapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+    }
+
+}
--- src/main/java/first/springframework/springwebapp/services/BookService.java
+++ src/main/java/first/springframework/springwebapp/services/BookService.java
@@ -1,7 +1,9 @@
 package first.springframework.springwebapp.services;
 
 import first.springframework.springwebapp.domain.Book;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface BookService {
 
     Iterable<Book> findAll();
